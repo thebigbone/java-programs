e106d7b94e9dcf7c0a13e5f872821bcb76c77e7b
thebigbone <pacman@duck.com>
2023-03-30 14:22:58 +0530

    used different classes

--- Misc/hashing/Hashing.java
+++ Misc/hashing/Hashing.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 import org.apache.commons.codec.digest.DigestUtils;
 
-public class Hashing {
+class Hashes {  
+    String input, salt, result;
+    int choice;
+    
+    Scanner scan = new Scanner(System.in);        
 
-    public static void main(String[] args) {
-        String input, salt, result;
-        int choice;
-
-        Scanner scan = new Scanner(System.in);        
+    void get_string() {
         try {
             System.out.println("Enter a string: ");
             input = scan.nextLine();
@@ -16,33 +16,61 @@ public class Hashing {
                 System.out.println("Invalid input! Please enter a valid string.");
                 System.exit(0);
             }
-
-            System.out.println("Choose the below 2 options:\n 1. Salt\n 2. No Salt\n");
-            choice = scan.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter salt: ");
-                    scan.nextLine();
-                    salt = scan.nextLine();
-                    result = DigestUtils.md5Hex(input + salt);
-                    System.out.println("Salted MD5: " + result);
-                    break;
-            
-                case 2:
-                    result = DigestUtils.md5Hex(input);
-                    System.out.println("Unsalted MD5: " + result);
-                    break;
-
-                default:
-                    System.out.println("Something went wrong! Please try again.");
-            
-            throw new InputMismatchException();
-
-            }
+        throw new InputMismatchException();
         }
-
+        
         catch (InputMismatchException e) {
-            System.out.println("Input mismatch! Following error occurred: " + e);
+            System.out.println("Input mismatch! The following error occurred: " + e);
+        }
+    } 
+
+    void md5_salt() {
+            System.out.println("Enter salt: ");
+            salt = scan.nextLine();
+            result = DigestUtils.md5Hex(input + salt);
+            System.out.println("Salted MD5: " + result);
+    }
+    
+    void md5() {
+        result = DigestUtils.md5Hex(input);
+        System.out.println("Unsalted MD5: " + result);
+    }
+    
+    void sha256() {
+        result = DigestUtils.sha256Hex(input);
+        System.out.println("SHA256: " + result);
+    }
+}
+
+public class Hashing {
+    public static void main(String[] args) {
+        Hashes h = new Hashes();
+        int choose;
+        Scanner scan = new Scanner(System.in);        
+        h.get_string();
+        System.out.println("Enter which hashing algorithm to use:\n 1. MD5\n 2. SHA256\n");
+        choose = scan.nextInt();
+
+        switch(choose) {
+            case 1:
+                System.out.println("Salt (1) or No salt (2): ");
+                int ch = scan.nextInt();
+                switch(ch) {
+                    case 1:
+                        h.md5_salt();
+                        break;
+                    case 2:
+                        h.md5();
+                        break;
+                    default:
+                        System.out.println("Something went wrong! Please try again.");
+                }
+                break;
+            case 2:
+                h.sha256();
+                break;
+            default:
+                System.out.println("Something went wrong! Please try again.");
         }
     }
 }
